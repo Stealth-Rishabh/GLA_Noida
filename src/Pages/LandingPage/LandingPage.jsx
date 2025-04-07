39fa95137a8dc11e5e9f5385b543626812fd38db
Yogesh Nandal <yogidesign1996@gmail.com>
2025-04-07 13:08:44 +0530

    feat: add Explore Majors and Student Life sections to LandingPage, update background image and styles

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,8 @@ import { BannerSlider } from "./banner-slider";
 import UndergraduateAdmissions from "@/components/main/UndergraduateAdmissions";
 import Section from "@/layout.jsx/section";
 import SetUsApart from "@/components/main/SetUsApart";
+import ExploreMajorSections from "@/components/main/ExploreMajorSections";
+import StudentLife from "@/components/main/StudentLife";
 
 export default function LandingPage() {
   return (
@@ -12,6 +14,10 @@ export default function LandingPage() {
       </Section>
       <Section className="background-gradient">
         <SetUsApart />
+      </Section>
+      <ExploreMajorSections />
+      <Section>
+        <StudentLife />
       </Section>
     </>
   );
