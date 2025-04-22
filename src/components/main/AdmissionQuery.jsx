93c70d86d33e668f77fcac3958f23bde7e611464
Danish <ammarsaifi5376@gmail.com>
2025-04-22 16:22:16 +0530

    chore: comment out unused input section in AdmissionQuery component for future reference

--- src/components/main/AdmissionQuery.jsx
+++ src/components/main/AdmissionQuery.jsx
@@ -277,7 +277,7 @@ export default function AdmissionQuery() {
                     </div>
                   ))}
 
-                  <div className="flex gap-2 mt-1">
+                  {/* <div className="flex gap-2 mt-1">
                     <div className="flex-1">
                       <input
                         type="text"
@@ -316,7 +316,7 @@ export default function AdmissionQuery() {
                         <path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                       </svg>
                     </button>
-                  </div>
+                  </div> */}
 
                   <button
                     type="submit"
